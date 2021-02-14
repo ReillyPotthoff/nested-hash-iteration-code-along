0dc480bfebdb1188d7423fd75117bb52fe53bf73
reillypotthoff <reillypotthoff@gmail.com>
2021-02-14 14:02:28 -0600

    commit tree 3cc55b84d13a4773373d85a592777463d1b40b72

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,16 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
-
-end
+  def remove_strawberry(contacts)
+    contacts.each do |name, value|
+      if  name == "Freddy Mercury"
+      value.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
+  
+  
+  end
